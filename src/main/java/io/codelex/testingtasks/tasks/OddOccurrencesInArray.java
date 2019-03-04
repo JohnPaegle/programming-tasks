9832134df24d468fbb21856414ed901ce40d11d0
janis <paegle3@gmail.com>
2019-03-04 15:35:43 +0200

    init

--- src/main/java/io/codelex/testingtasks/tasks/OddOccurrencesInArray.java
+++ src/main/java/io/codelex/testingtasks/tasks/OddOccurrencesInArray.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class OddOccurrencesInArray {
    
     public static void main(String[] args) {
-        System.out.println(solution3(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+        System.out.println(solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
 
     }
 
@@ -19,34 +19,15 @@ public class OddOccurrencesInArray {
     }
 
     public static int solution2(int[] A) {
-        HashSet<Integer> in = new HashSet<Integer>();
+        HashSet<Integer> input = new HashSet<Integer>();
         int element = -1;
 
         for (int i = 0; i < A.length; i++) {
-            if (in.add(A[i])) {
+            if (input.add(A[i])) {
                 element = A[i];
             }
         }
         return element;
     }
     
-    public static int solution3(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-
-            for (int j = 0; j < A.length; j++) {
-                if (A[i] == A[j] && i != j) {
-                    A[i] = -1;
-                    A[j] = -1;
-                    j = A.length;
-                }
-            }
-        }
-
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] != -1)
-                return A[i];
-        }
-
-        return 0; 
-    }
 }
